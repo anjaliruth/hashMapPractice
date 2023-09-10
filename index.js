3d626af045d5fe226603e42ebfb522c0dccb3e8a
anjaliruth <ruthanjali0211@gmail.com>
2023-09-10 13:48:14 +0100

    3 questions done

--- index.js
+++ index.js
@@ -10,22 +10,100 @@
 function findComplement(nums, target) {
   //create a map to store number and indices
   const myMap = {};
-  
-  for (let i = 0; i < nums.length; i++) {
 
-    const currentNum = nums[i]
-    const complement = target - nums[i]
+  for (let i = 0; i < nums.length; i++) {
+    const currentNum = nums[i];
+    const complement = target - nums[i];
 
     if (myMap.hasOwnProperty(complement)) {
-        return [complement, currentNum]
+      return [complement, currentNum];
+    } else {
+      myMap[currentNum] = i;
     }
-
-    else{
-        myMap[currentNum] = i
-    }
-
   }
-  return []
+  return [];
 }
 
-module.exports = findComplement
+//Question 2
+// Word Frequency Counter:
+// Given a list of words, count the frequency of each word in the list and return the results as a dictionary.
+
+// Example:
+// Input: ["apple", "banana", "apple", "cherry", "banana"]
+// Output: {"apple": 2, "banana": 2, "cherry": 1}
+
+//Plan:
+//create map
+//for loop to go through input
+//if its in the input, then myMap(string) = myMap(string) + 1
+
+function getCount(input) {
+  const myMap = {};
+
+  for (let i = 0; i < input.length; i++) {
+    const current = input[i];
+
+    if (myMap.hasOwnProperty(current)) {
+      myMap[current] = myMap[current] + 1;
+    } else {
+      myMap[current] = 1;
+    }
+  }
+  return myMap;
+}
+
+//Longest Substring Without Repeating Characters:
+// Given a string, find the length of the longest substring without repeating characters.
+
+// Example:
+
+// Input: "abcabcbb"
+// Output: 3 (The longest substring without repeating characters is "abc").
+
+//create answer string
+//iterate over input, if not in string, add it to string
+//once in string, break
+//return string
+
+// function longestString(input) {
+//   let answer = "";
+//   let currentSubstring = "";
+
+//   for (let i = 0; i < input.length; i++) {
+//     if (!currentSubstring.includes(input[i])) {
+//       currentSubstring += input[i];
+//       if (currentSubstring.length > answer.length) {
+//         answer = currentInput;
+//       }
+//     } else {
+//       currentSubstring = input[i];
+//     }
+//   }
+//   return `The longest substring without repeating characters is ${answer}`;
+// }
+
+function longestString(input) {
+    let longestSubstring = "";
+    let charMap= {}
+    let start = 0
+    let maxLength = 0
+
+    for (let end = 0; end < input.length; end++) {
+
+        const currentChar = input[end]
+        if (charMap[end] !== undefined && charMap[currentChar] >= start) {
+            start = charMap[currentChar] + 1
+        }
+        //add unseen letter to the map with its index
+        charMap[currentChar] = end
+
+        if (end-start + 1 > maxLength) {
+            maxLength = end - start + 1
+            longestString = input.slice(start, end + 1)
+        }
+
+    }
+    return longestSubstring
+}
+
+module.exports = { findComplement, getCount, longestString };
--- index.test.js
+++ index.test.js
@@ -1,11 +1,27 @@
-const findComplement = require("./index.js");
+const { findComplement, getCount, longestString } = require("./index.js");
 
+describe("findComplement", () => {
+  it("should return [2,7] for [2,7,11,15] for target 9", () => {
+    const nums = [2, 7, 11, 15];
+    const target = 9;
+    const result = findComplement(nums, target);
+    expect(result).toEqual([2, 7]);
+  });
+});
 
-describe("findComplement", ()=> {
-it ("should return [2,7] for [2,7,11,15] for target 9", ()=>{
-    const nums = [2,7,11,15]
-    const target = 9
-    const result = findComplement(nums, target)
-    expect(result).toEqual([2,7])
-})
-})
+describe("getCount", () => {
+  it(`should return {"apple": 2, "banana": 2, "cherry": 1} for input of ["apple", "banana", "apple", "cherry", "banana"]`, () => {
+    const input = ["apple", "banana", "apple", "cherry", "banana"];
+    const expected = { apple: 2, banana: 2, cherry: 1 };
+    const actual = getCount(input);
+    expect(actual).toEqual(expected);
+  });
+});
+describe("longestString", () => {
+  it(`should return The longest substring without repeating characters is abc`, () => {
+    const input = "abcabcbb";
+    const expected = "The longest substring without repeating characters is abc";
+    const actual = longestString(input);
+    expect(actual).toEqual(expected);
+  });
+});
