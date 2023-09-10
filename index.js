7f165e19431d9c106385584edf6d0bc22ae2846b
anjaliruth <ruthanjali0211@gmail.com>
2023-09-10 11:48:39 +0100

    added q1

---
+++ index.js
@@ -0,0 +1,9 @@
+// Quesiton 1:
+// Two Sum:
+// Given an array of integers and a target sum, find two numbers in the array that add up to the target sum.
+
+// Example:
+
+// Input: [2, 7, 11, 15], target = 9
+// Output: [2, 7]
+
